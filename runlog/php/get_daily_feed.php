d823d0309f2f051d7d2810c993f2224a1a0ddf26
gaby.marcus@gmail.com <gaby.marcus@gmail.com@9a0e9ef8-c382-0d49-0163-095ddfe4c003>
2013-03-31 17:56:24 +0000

    showing new comments since last visit

--- runlog/php/get_daily_feed.php
+++ runlog/php/get_daily_feed.php
@@ -15,7 +15,15 @@ catch(PDOException $e) {
 
 // Fetch all the events of a given date from DB as JSON
 function getDayEventsAsJSON($conn, $date) {
-    $sql = "select tl_events.id, tl_runners.member_name as 'name', tl_run_types.type, run_distance, run_time, warmup_distance, cooldown_distance, COALESCE(tl_events.notes, '') as 'notes', tl_events.run_type_id from tl_runners, tl_events, tl_run_types where tl_runners.id = tl_events.runner_id and tl_events.run_date = '" . $date . "' and tl_events.run_type_id = tl_run_types.id order by tl_events.id desc";
+    $sql =
+
+"SELECT tl_events.id, tl_runners.member_name AS 'name', tl_run_types.type, run_distance, run_time, warmup_distance, cooldown_distance, COALESCE(tl_events.notes, '') AS 'notes', tl_events.run_type_id
+    FROM tl_runners, tl_events, tl_run_types
+    WHERE tl_runners.id = tl_events.runner_id
+        AND tl_events.run_date = '" . $date . "'
+        AND tl_events.run_type_id = tl_run_types.id
+    ORDER BY tl_events.id DESC";
+
     $stmt = $conn->query($sql);
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
---
+++ runlog/php/get_team_new_comments.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once 'ajax_page_init.php';
+$runnerId = $memberAuthentication->getMemberId();
+$timestamp = '0000-00-00 00:00:00';
+if (isset($_GET['timestamp'])){
+    $timestamp = $_GET['timestamp'];
+}
+
+try {
+    $conn = getConnection();
+
+    $sql =
+
+"SELECT DISTINCT tl_events.id, tl_events.run_date, tl_runners.member_name AS 'name', tl_run_types.type, run_distance, run_time, warmup_distance, cooldown_distance, COALESCE(tl_events.notes, '') AS 'notes', tl_events.run_type_id
+    FROM tl_runners, tl_events, tl_run_types, tl_comments
+    WHERE tl_runners.id = tl_events.runner_id
+        AND tl_events.run_type_id = tl_run_types.id
+        AND tl_comments.runner_id != '" . $runnerId . "'
+        AND tl_comments.timestamp >= '" . $timestamp . "'
+        AND tl_events.id = tl_comments.event_id
+    ORDER BY tl_events.id DESC";
+
+    $stmt = $conn->query($sql);
+    $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $eventIds = array();
+    foreach ($events as $event){
+        $eventIds[] = $event['id'];
+    }
+
+    $sql =
+
+"SELECT events.id AS event_id, comments.id AS comment_id, comments.runner_id, runners.member_name AS 'commenter_name', COALESCE(comments.comment, '') AS comment
+ FROM tl_events events
+    JOIN tl_comments comments ON events.id=comments.event_id
+    JOIN tl_runners runners ON comments.runner_id = runners.id
+ WHERE events.id IN (".implode(',', $eventIds).")
+ ORDER BY events.id DESC, comments.id ASC";
+
+    $stmt = $conn->query($sql);
+    $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $conn = null;
+
+    $result = array(
+        'events' => $events,
+        'comments' => $comments,
+    );
+
+    echo returnJSONsuccess($result);
+}
+catch(PDOException $e) {
+    die(getErrorStatusWithDummyData($e->getMessage()));
+}

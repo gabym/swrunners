e138b38876758234b5c111a16f20ad7363ac9529
gaby.marcus@gmail.com <gaby.marcus@gmail.com@9a0e9ef8-c382-0d49-0163-095ddfe4c003>
2013-04-03 14:32:56 +0000

    better loading of feed events and comments

--- trunk/runlog/php/get_daily_feed.php
+++ trunk/runlog/php/get_daily_feed.php
@@ -18,14 +18,31 @@ function getDayEventsAsJSON($conn, $date) {
     $sql =
 
 "SELECT tl_events.id, tl_runners.member_name AS 'name', tl_run_types.type, run_distance, run_time, warmup_distance, cooldown_distance, COALESCE(tl_events.notes, '') AS 'notes', tl_events.run_type_id
-    FROM tl_runners, tl_events, tl_run_types
-    WHERE tl_runners.id = tl_events.runner_id
-        AND tl_events.run_date = '" . $date . "'
-        AND tl_events.run_type_id = tl_run_types.id
+    FROM tl_events
+    JOIN tl_runners ON tl_events.runner_id = tl_runners.id
+    JOIN tl_run_types ON tl_events.run_type_id = tl_run_types.id
+    WHERE tl_events.run_date = '" . $date . "'
     ORDER BY tl_events.id DESC";
 
     $stmt = $conn->query($sql);
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql =
+
+"SELECT events.id AS event_id, comments.id AS comment_id, comments.runner_id, runners.member_name as 'commenter_name', COALESCE(comments.comment, '') AS comment
+ FROM tl_events events
+ JOIN tl_comments comments ON events.id = comments.event_id
+ JOIN tl_runners runners ON comments.runner_id = runners.id
+ WHERE events.run_date = '".$date."'
+ ORDER BY events.id DESC, comments.id ASC";
+
+    $stmt = $conn->query($sql);
+    $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $result = array(
+        'events' => $events,
+        'comments' => $comments,
+    );
 
     return returnJSONsuccess($result);
 }

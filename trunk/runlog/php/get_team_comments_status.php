08ac70fa6090227eb4c4f08b382134f73c8b050f
gaby.marcus@gmail.com <gaby.marcus@gmail.com@9a0e9ef8-c382-0d49-0163-095ddfe4c003>
2013-04-23 06:15:13 +0000

    when never visited new comments, show new comments from last 48 hours only

--- trunk/runlog/php/get_team_comments_status.php
+++ trunk/runlog/php/get_team_comments_status.php
@@ -12,13 +12,14 @@ try {
     $teamCommentsLastFetchedByRunner = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     if (count($teamCommentsLastFetchedByRunner) == 0){
+        // never viewed new comments before - show comments from the last 24 hrs
+        $timestamp = date("Y-m-d H:i:s", strtotime('-2 day'));
         $teamCommentsLastFetchedByRunner = array(
             0 => array(
-                'last_fetched' => '0000-00-00 00:00:00',
+                'last_fetched' => $timestamp,
             ),
         );
     }
-
 
     $sql = "SELECT count(*) AS count_new_comments FROM tl_comments WHERE runner_id != '" . $runnerId . "' AND timestamp >= '" . $teamCommentsLastFetchedByRunner[0]['last_fetched'] . "'";
     $stmt = $conn->query($sql);

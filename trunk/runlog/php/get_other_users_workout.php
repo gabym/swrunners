8990b4b8a907d16f7259a7cd980f530cab9a34dc
gaby.marcus@gmail.com <gaby.marcus@gmail.com@9a0e9ef8-c382-0d49-0163-095ddfe4c003>
2013-04-27 03:01:36 +0000

    cleanup

--- trunk/runlog/php/get_other_users_workout.php
+++ trunk/runlog/php/get_other_users_workout.php
@@ -21,7 +21,15 @@ catch(PDOException $e) {
 
 // Fetch all the events of a given date from DB as JSON
 function getDayEventsAsJSON($conn, $date) {
-    $sql = "select tl_runners.member_name as 'name', tl_run_types.type, run_distance, run_time, warmup_distance, cooldown_distance, COALESCE(tl_events.notes, '') as 'notes', tl_events.run_type_id from tl_runners, tl_events, tl_run_types where tl_runners.id = tl_events.runner_id and tl_events.run_date = '" . $date . "' and tl_events.run_type_id = tl_run_types.id order by tl_events.id asc";
+    $sql =
+
+"SELECT tl_runners.member_name as 'name', tl_run_types.type, run_distance, run_time, warmup_distance, cooldown_distance, COALESCE(tl_events.notes, '') as 'notes', tl_events.run_type_id
+    FROM tl_runners, tl_events, tl_run_types
+    WHERE tl_runners.id = tl_events.runner_id
+        AND tl_events.run_date = '" . $date . "'
+        AND tl_events.run_type_id = tl_run_types.id
+    ORDER BY tl_events.id ASC";
+
     $stmt = $conn->query($sql);
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
--- trunk/runlog/php/get_team_events.php
+++ trunk/runlog/php/get_team_events.php
@@ -16,7 +16,16 @@ catch(PDOException $e) {
 
 // Fetch the events info from DB as JSON
 function getTeamEventsAsJSON($conn, $start_date, $end_date) {
-    $sql = "SELECT tl_events.id as id, tl_run_types.type, run_distance, run_time, warmup_distance, cooldown_distance, COALESCE(notes, '') AS notes, run_date AS start, tl_run_types.id as run_type_id, runner_id, tl_runners.member_name as runner_name FROM tl_events, tl_run_types, tl_runners WHERE tl_run_types.id = tl_events.run_type_id and run_date >= '".$start_date."' and run_date <= '".$end_date."' and runner_id = tl_runners.id ORDER BY runner_name, run_date";
+    $sql =
+
+"SELECT tl_events.id AS id, tl_run_types.type, run_distance, run_time, warmup_distance, cooldown_distance, COALESCE(notes, '') AS notes, run_date AS start, tl_run_types.id AS run_type_id, runner_id, tl_runners.member_name AS runner_name
+    FROM tl_events, tl_run_types, tl_runners
+    WHERE tl_run_types.id = tl_events.run_type_id
+        AND run_date >= '".$start_date."'
+        AND run_date <= '".$end_date."'
+        AND runner_id = tl_runners.id
+    ORDER BY runner_name, run_date";
+
     $stmt = $conn->query($sql);
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
